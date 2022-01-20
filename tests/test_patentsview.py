bc0c6d64ce68322d8c3f59c2d65ae1e9c726ffcb
EliaPaccagnella <879028@stud.unive.it>
2022-01-20 14:56:46 +0100

    Adding tests for invalid input for make_request (to be updated with error testing)

--- tests/test_patentsview.py
+++ tests/test_patentsview.py
@@ -24,7 +24,25 @@ class test_make_request(unittest.TestCase):
                         json.loads('{"patents":[{"patent_id":"10524528"},{"patent_id":"10524914"},{"patent_id":"10525006"},{"patent_id":"10525072"},{"patent_id":"10525161"},{"patent_id":"10525429"},{"patent_id":"10525432"},{"patent_id":"10525653"},{"patent_id":"10525755"},{"patent_id":"10525948"},{"patent_id":"10525990"},{"patent_id":"10526053"},{"patent_id":"10526156"},{"patent_id":"10526365"},{"patent_id":"10526442"},{"patent_id":"10526735"},{"patent_id":"10526781"},{"patent_id":"10526794"},{"patent_id":"10526827"},{"patent_id":"10526892"},{"patent_id":"10526926"},{"patent_id":"10527052"},{"patent_id":"10527206"},{"patent_id":"10527209"},{"patent_id":"10527259"}],"count":25,"total_patent_count":2596}'))
 
     def test_wrong_input(self):
-        pass
+        # testing wrong endpoint error
+        self.req.make_request(
+            endpoint='wrong_endpoint',
+            query='{"patent_number":"7861215"}'
+        )
+        # should raise a Endpoint Error, implement test for error
+
+        # testing wrong query error
+        self.req.make_request(
+            query='{"patent_number:"7861215"}' # forgot closing "
+        )
+        # should raise a Syntax Error, implement test for error
+
+        # testing wrong fields error
+        self.req.make_request(
+            query='{"patent_number":"7861215"}',
+            fields=["patent_id","invntor_idd"] # inventor misspelling
+            )
+        # should raise a Syntax Error, implement test for error
 
     def test_corner_input(self):
         pass
