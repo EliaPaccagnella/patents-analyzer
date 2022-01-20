c251b0b42d67c4e5000297bfd8a81d1ef8a8e4cd
EliaPaccagnella <879028@stud.unive.it>
2022-01-20 19:30:59 +0100

    Fixing testing error due to API update

--- tests/test_patentsview.py
+++ tests/test_patentsview.py
@@ -1,8 +1,9 @@
-from dataclasses import fields
 import unittest
 import json
 import sys
 import os
+from unittest.mock import patch
+import io
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from analyzer import patentsview as api
 from analyzer.errors import errors
@@ -11,12 +12,12 @@ class test_make_request(unittest.TestCase):
 
     def setUp(self):
         self.req = api.Request()
-        print("Initializing test...")
+        print("\nInitializing test...")
         return
 
     def tearDown(self):
         del self.req
-        print("Tests run")
+        print("Tests result:")
         return
 
     def test_valid_input(self):
@@ -67,57 +68,47 @@ class test_make_request(unittest.TestCase):
             ]
         )
         # ---> checking the results
-        result = ('{"patents":[{"patent_number":"10006771",'
-                  '"patent_processing_time":"698","patent_kind":"B2"},'
-                  '{"patent_number":"10011146",'
-                  '"patent_processing_time":"1800","patent_kind":"B2"},'
-                  '{"patent_number":"10050913",'
-                  '"patent_processing_time":"1700","patent_kind":"B2"},'
-                  '{"patent_number":"10064048",'
-                  '"patent_processing_time":"235","patent_kind":"B1"},'
-                  '{"patent_number":"10067572",'
-                  '"patent_processing_time":"2665","patent_kind":"B2"},'
-                  '{"patent_number":"10157373",'
-                  '"patent_processing_time":"497","patent_kind":"B1"},'
-                  '{"patent_number":"10157374",'
-                  '"patent_processing_time":"160","patent_kind":"B1"},'
-                  '{"patent_number":"10169528",'
-                  '"patent_processing_time":"2114","patent_kind":"B2"},'
-                  '{"patent_number":"10182341",'
-                  '"patent_processing_time":"351","patent_kind":"B2"},'
-                  '{"patent_number":"10210177",'
-                  '"patent_processing_time":"894","patent_kind":"B2"},'
-                  '{"patent_number":"10237724",'
-                  '"patent_processing_time":"755","patent_kind":"B2"},'
-                  '{"patent_number":"10258654",'
-                  '"patent_processing_time":"568","patent_kind":"B2"},'
-                  '{"patent_number":"10274407",'
-                  '"patent_processing_time":"354","patent_kind":"B2"},'
-                  '{"patent_number":"10285100",'
-                  '"patent_processing_time":"1477","patent_kind":"B2"},'
-                  '{"patent_number":"10312419",'
-                  '"patent_processing_time":"539","patent_kind":"B2"},'
-                  '{"patent_number":"10321301",'
-                  '"patent_processing_time":"1309","patent_kind":"B2"},'
-                  '{"patent_number":"10325062",'
-                  '"patent_processing_time":"844","patent_kind":"B2"},'
-                  '{"patent_number":"10327140",'
-                  '"patent_processing_time":"778","patent_kind":"B2"},'
-                  '{"patent_number":"10335304",'
-                  '"patent_processing_time":"1664","patent_kind":"B2"},'
-                  '{"patent_number":"10339733",'
-                  '"patent_processing_time":"575","patent_kind":"B2"},'
-                  '{"patent_number":"10354469",'
-                  '"patent_processing_time":"809","patent_kind":"B2"},'
-                  '{"patent_number":"10382958",'
-                  '"patent_processing_time":"1474","patent_kind":"B2"},'
-                  '{"patent_number":"10391966",'
-                  '"patent_processing_time":"775","patent_kind":"B2"},'
-                  '{"patent_number":"10395449",'
-                  '"patent_processing_time":"197","patent_kind":"B2"},'
-                  '{"patent_number":"10395450",'
-                  '"patent_processing_time":"197","patent_kind":"B2"}],'
-                  '"count":25,"total_patent_count":484}')
+        result = (
+            '{"patents":[{"patent_number":"10006771",'
+            '"patent_processing_time":"698","patent_kind":"B2"},'
+            '{"patent_number":"10011146","patent_processing_time":"1800",'
+            '"patent_kind":"B2"},{"patent_number":"10050913",'
+            '"patent_processing_time":"1700","patent_kind":"B2"},'
+            '{"patent_number":"10067572","patent_processing_time":"2665",'
+            '"patent_kind":"B2"},{"patent_number":"10157373",'
+            '"patent_processing_time":"497","patent_kind":"B1"},'
+            '{"patent_number":"10157374","patent_processing_time":"160",'
+            '"patent_kind":"B1"},{"patent_number":"10169528",'
+            '"patent_processing_time":"2114","patent_kind":"B2"},'
+            '{"patent_number":"10182341","patent_processing_time":"351",'
+            '"patent_kind":"B2"},{"patent_number":"10210177",'
+            '"patent_processing_time":"894","patent_kind":"B2"},'
+            '{"patent_number":"10237724","patent_processing_time":"755",'
+            '"patent_kind":"B2"},{"patent_number":"10258654",'
+            '"patent_processing_time":"568","patent_kind":"B2"},'
+            '{"patent_number":"10274407","patent_processing_time":"354",'
+            '"patent_kind":"B2"},{"patent_number":"10285100",'
+            '"patent_processing_time":"1477","patent_kind":"B2"},'
+            '{"patent_number":"10312419","patent_processing_time":"539",'
+            '"patent_kind":"B2"},{"patent_number":"10321301",'
+            '"patent_processing_time":"1309","patent_kind":"B2"},'
+            '{"patent_number":"10325062","patent_processing_time":"844",'
+            '"patent_kind":"B2"},{"patent_number":"10327140",'
+            '"patent_processing_time":"778","patent_kind":"B2"},'
+            '{"patent_number":"10335304","patent_processing_time":"1664",'
+            '"patent_kind":"B2"},{"patent_number":"10339733",'
+            '"patent_processing_time":"575","patent_kind":"B2"},'
+            '{"patent_number":"10354469","patent_processing_time":"809",'
+            '"patent_kind":"B2"},{"patent_number":"10382958",'
+            '"patent_processing_time":"1474","patent_kind":"B2"},'
+            '{"patent_number":"10395449","patent_processing_time":"197",'
+            '"patent_kind":"B2"},{"patent_number":"10395450",'
+            '"patent_processing_time":"197","patent_kind":"B2"},'
+            '{"patent_number":"10395451","patent_processing_time":"197",'
+            '"patent_kind":"B2"},{"patent_number":"10410179",'
+            '"patent_processing_time":"837","patent_kind":"B2"}],'
+            '"count":25,"total_patent_count":476}'
+        )
         self.assertEqual(self.req._Request__data, json.loads(result))
 
         # testing inventors endpoint
@@ -128,32 +119,34 @@ class test_make_request(unittest.TestCase):
             fields=["inventor_id"]
         )
         # ---> checking the results
-        result = ('{"inventors":[{"inventor_id":"fl:t_ln:young-48"},'
-                  '{"inventor_id":"fl:j_ln:young-123"},'
-                  '{"inventor_id":"fl:r_ln:young-73"},'
-                  '{"inventor_id":"fl:r_ln:young-105"},'
-                  '{"inventor_id":"fl:j_ln:young-70"},'
-                  '{"inventor_id":"fl:t_ln:young-92"},'
-                  '{"inventor_id":"fl:f_ln:young-22"},'
-                  '{"inventor_id":"fl:v_ln:young-14"},'
-                  '{"inventor_id":"fl:r_ln:young-155"},'
-                  '{"inventor_id":"fl:d_ln:young-86"},'
-                  '{"inventor_id":"fl:a_ln:young-73"},'
-                  '{"inventor_id":"fl:k_ln:young-92"},'
-                  '{"inventor_id":"fl:c_ln:young-66"},'
-                  '{"inventor_id":"fl:j_ln:young-32"},'
-                  '{"inventor_id":"fl:d_ln:young-45"},'
-                  '{"inventor_id":"fl:j_ln:young-41"},'
-                  '{"inventor_id":"fl:j_ln:young-184"},'
-                  '{"inventor_id":"fl:j_ln:young-218"},'
-                  '{"inventor_id":"fl:m_ln:young-36"},'
-                  '{"inventor_id":"fl:c_ln:young-128"},'
-                  '{"inventor_id":"fl:c_ln:young-19"},'
-                  '{"inventor_id":"fl:b_ln:young-46"},'
-                  '{"inventor_id":"fl:w_ln:young-76"},'
-                  '{"inventor_id":"fl:t_ln:young-17"},'
-                  '{"inventor_id":"fl:w_ln:young-14"}],'
-                  '"count":25,"total_inventor_count":1952}')
+        result = (
+            '{"inventors":[{"inventor_id":"fl:t_ln:young-48"},'
+            '{"inventor_id":"fl:j_ln:young-123"},'
+            '{"inventor_id":"fl:r_ln:young-73"},'
+            '{"inventor_id":"fl:r_ln:young-105"},'
+            '{"inventor_id":"fl:j_ln:young-70"},'
+            '{"inventor_id":"fl:t_ln:young-92"},'
+            '{"inventor_id":"fl:f_ln:young-22"},'
+            '{"inventor_id":"fl:v_ln:young-14"},'
+            '{"inventor_id":"fl:r_ln:young-155"},'
+            '{"inventor_id":"fl:d_ln:young-86"},'
+            '{"inventor_id":"fl:a_ln:young-73"},'
+            '{"inventor_id":"fl:k_ln:young-92"},'
+            '{"inventor_id":"fl:c_ln:young-66"},'
+            '{"inventor_id":"fl:j_ln:young-32"},'
+            '{"inventor_id":"fl:d_ln:young-45"},'
+            '{"inventor_id":"fl:j_ln:young-41"},'
+            '{"inventor_id":"fl:j_ln:young-184"},'
+            '{"inventor_id":"fl:j_ln:young-218"},'
+            '{"inventor_id":"fl:m_ln:young-36"},'
+            '{"inventor_id":"fl:c_ln:young-128"},'
+            '{"inventor_id":"fl:c_ln:young-19"},'
+            '{"inventor_id":"fl:b_ln:young-46"},'
+            '{"inventor_id":"fl:w_ln:young-76"},'
+            '{"inventor_id":"fl:t_ln:young-17"},'
+            '{"inventor_id":"fl:w_ln:young-14"}],'
+            '"count":25,"total_inventor_count":1964}'
+        )
         self.assertEqual(self.req._Request__data, json.loads(result))
 
         # testing assignees endpoint
@@ -164,33 +157,35 @@ class test_make_request(unittest.TestCase):
             fields=["assignee_id"]
         )
         # ---> checking the results
-        result = ('{"assignees":['
-                  '{"assignee_id":"ef719d1c-52bd-4bcf-8a1e-1dba54f71f76"},'
-                  '{"assignee_id":"688156dd-0ba8-488a-bf29-e748dc9f55b9"},'
-                  '{"assignee_id":"ae884e9f-64ea-4c6a-b2d2-dc3f83a32f1f"},'
-                  '{"assignee_id":"5ef1ebe7-93ba-4b8e-9c78-00227e355c99"},'
-                  '{"assignee_id":"f1386c77-acb0-4fe1-9318-04fc3b6cb706"},'
-                  '{"assignee_id":"1ee6c4dc-cba7-4cb1-a97b-ea1f363314df"},'
-                  '{"assignee_id":"336378ae-dba8-46c8-a21c-a9fc9548e9a8"},'
-                  '{"assignee_id":"05c63b8e-670d-445a-995a-df9d16adb8b0"},'
-                  '{"assignee_id":"d0f5c21b-0559-44dd-ad13-169603968e32"},'
-                  '{"assignee_id":"3855ccf0-089d-4d6d-a712-936263c7781f"},'
-                  '{"assignee_id":"cb9a3fd3-2bb3-409c-9e23-0cb4c9e5250a"},'
-                  '{"assignee_id":"84018e15-0b50-4c93-8300-a879eb08fbec"},'
-                  '{"assignee_id":"757402a4-74fc-48f4-b466-592b4c0f966c"},'
-                  '{"assignee_id":"764c39c7-574a-4d6d-85c2-cc9fc6a60705"},'
-                  '{"assignee_id":"59d0e158-d526-4315-b0ec-c064253c75fb"},'
-                  '{"assignee_id":"5c958e1d-f60a-4b25-ade1-810cef1522a8"},'
-                  '{"assignee_id":"1adfdd0b-91c5-488b-aac1-724b1aad2700"},'
-                  '{"assignee_id":"a0c79ab1-9507-4c6c-882e-0a99268aba7d"},'
-                  '{"assignee_id":"99448ea6-4ece-4081-a19a-24c4ce9c9050"},'
-                  '{"assignee_id":"20952a1d-62e3-4048-9bd3-68583c7b51e7"},'
-                  '{"assignee_id":"76c0aff0-0d40-48c8-9204-9d043b8d9401"},'
-                  '{"assignee_id":"1e810460-3ed0-4e00-94e7-d23a6960417c"},'
-                  '{"assignee_id":"0722c2dd-9459-47cf-ba9b-891b140e7cd7"},'
-                  '{"assignee_id":"2603e2be-a035-43cf-88b3-c25b7a5f09df"},'
-                  '{"assignee_id":"0e4a8be0-77bd-47ff-ba66-2db32394cd65"}]'
-                  ',"count":25,"total_assignee_count":36}')
+        result = (
+            '{"assignees":['
+            '{"assignee_id":"f397a05a-896d-43f4-8223-f251e28cac03"},'
+            '{"assignee_id":"f0cb3bd6-d1f4-4bad-87d9-4747b3ba352f"},'
+            '{"assignee_id":"425c27fc-ccc3-4663-b3c3-d2ef3b2d404e"},'
+            '{"assignee_id":"6a684fa2-a929-4ae7-b845-6be6b21bad64"},'
+            '{"assignee_id":"6d6559a7-777a-49c6-b9e2-e36a32774a41"},'
+            '{"assignee_id":"6f93d51f-377b-4f33-b8cf-e3326b9a8706"},'
+            '{"assignee_id":"165251ba-359d-4d2d-b11f-eb367b0c0d31"},'
+            '{"assignee_id":"f0d968f0-8e6d-4d0e-a5df-c8fc6d27cd3d"},'
+            '{"assignee_id":"b040caaa-5637-47e6-9a6c-1f14b7559c28"},'
+            '{"assignee_id":"ae61403d-7c9a-49e3-961c-a30c690992eb"},'
+            '{"assignee_id":"e2eb96bf-caa8-4e77-8587-27d542559b8a"},'
+            '{"assignee_id":"3cddca88-0949-41a2-a9f9-5f9894fd5f43"},'
+            '{"assignee_id":"6cfce6f2-6afd-408a-a73e-d89da50f3bf9"},'
+            '{"assignee_id":"97ab8a08-0733-4a44-8eb3-6cddaf065b80"},'
+            '{"assignee_id":"96a6943f-8d7f-455c-97ea-029c42ce79fb"},'
+            '{"assignee_id":"2e49a438-bbfb-4c2c-9343-661b9429a842"},'
+            '{"assignee_id":"2c4953e2-8302-4e27-bbe2-b9e4b3bdcb1e"},'
+            '{"assignee_id":"118ac57a-60f9-4694-b1a8-0c53328ae72b"},'
+            '{"assignee_id":"e1bfc0e0-03e8-49eb-8499-9c54e7070c75"},'
+            '{"assignee_id":"45f96f16-0c1b-457a-9979-7b5551e7d3bb"},'
+            '{"assignee_id":"f1927c05-da5e-42a8-a8c7-90ba16023a62"},'
+            '{"assignee_id":"4b612f6e-9a99-4cec-9b97-2a22d3ccad6f"},'
+            '{"assignee_id":"746abc96-0bfa-4141-ae43-2ead67f1227c"},'
+            '{"assignee_id":"7a5744b8-828c-47c8-900b-cf144719f068"},'
+            '{"assignee_id":"5ce7ffd5-3c95-4cfc-a35b-abe71a368363"}],'
+            '"count":25,"total_assignee_count":36}'
+        )
         self.assertEqual(self.req._Request__data, json.loads(result))
 
         # testing locations endpoint
@@ -266,12 +261,12 @@ class test_get_data(unittest.TestCase):
 
     def setUp(self):
         self.req = api.Request()
-        print("Initializing test...")
+        print("\nInitializing test...")
         return
     
     def tearDown(self):
         del self.req
-        print("Tests run")
+        print("Tests result")
         return
 
     def test_valid_input(self):
