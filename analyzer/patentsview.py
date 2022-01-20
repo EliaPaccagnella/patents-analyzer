dcd891c8a53077412726152567cf4ea95cdf1de6
EliaPaccagnella <879028@stud.unive.it>
2022-01-20 20:01:50 +0100

    Adding documentation to patentsview.py

--- analyzer/patentsview.py
+++ analyzer/patentsview.py
@@ -8,6 +8,23 @@ class Request():
         self.__data = None
 
     def make_request(self, endpoint='patents', query=None, fields=''):
+        '''Request data from the PatentsView API
+
+    INPUT:
+        • endpoint <str>: API endpoint to which you want to make requests.
+                          Valid values are: ['patents','inventors','assignees',
+                          'locations','cpc_subsections','uspc_mainclasses',
+                          'nber_subcategories']
+        • query <str>: query to the API as described in the API Documentation
+        • fields <list>: fields to include in the results of the API
+
+    OUTPUT:
+        The function doesn't return directly the data. To obtain it, use the 
+        get_data method after making the request.
+        If errors arise during the data request, this function will raise
+        instead python errors signaling the issue.
+        '''
+
         # defining basic URL syntax
         self.__request_url = 'https://api.patentsview.org/{endpoint}/query?q={query}&f={fields}'
         
@@ -47,6 +64,7 @@ class Request():
                 try:
                     if json_data['count'] == 0:
                         # there is no data for the query
+                        # printing a small warning
                         print(('\033[93m'
                                'There is no data for your query'
                                '\033[37m'))
@@ -54,7 +72,7 @@ class Request():
                         return
                 except KeyError:
                     if json_data['status'] == 'error':
-                        # there was a syntax error
+                        # there was a syntax error either in fields or query
                         raise errors.SyntaxError(
                             json_data['payload']['error']
                         )
@@ -70,6 +88,11 @@ class Request():
         return
 
     def get_data(self):
+        """Returns the data generated with make_request.
+
+        OUTPUT:
+            data <json>: last API request outputed data
+        """
         if self.__data is None:
             raise errors.NoData()
         return self.__data
