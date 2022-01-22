7b3b92f13bd773dfd8c277dc50de22e6fdc176d2
SofiaChen21 <880227@stud.unive.it>
2022-01-22 11:18:22 +0100

    Adding some valid tests

--- tests/test_growth.py
+++ tests/test_growth.py
@@ -17,6 +17,11 @@ class Test_patents_country_years(unittest.TestCase):
         print("Test completed")
         return
 
+    def test_valid_input(self):
+        self.assertEqual(patents_country_years("IT", 1980, 1982), {'1980': 570, '1981': 672, '1982': 570})
+        self.assertEqual(patents_country_years("US", 1999, 2003),{'1999': 77726, '2000': 80677, '2001': 83971, '2002': 82758,'2003': 84432})
+        self.assertEqual(patents_country_years("JP", 1977, 1978),{'1977': 5839, '1978': 6558})
+
 
 if __name__=='__main__':
     unittest.main(verbosity=0)
