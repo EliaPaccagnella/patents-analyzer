64234e16d5acf9d20f80c4345a203773dc0d0a43
AnnaTonolo <95570933+AnnaTonolo@users.noreply.github.com>
2022-01-23 10:24:45 +0100

    Updated author feature, added comments

--- analyzer/author_feature.py
+++ analyzer/author_feature.py
@@ -3,37 +3,37 @@ import patentsview as api
 import json
 import sys
 
-def stampa_brevetti(name, surname, val, contents):
+def print_patents(name, surname, val, contents):
     match val:
-        case 'n':                                   # stampa numero di brevetti
-            print('\nIl numero di brevetti di ' + name + ' ' + surname + ' è: ' + str(contents['count']));
+        case 'n':                                   # print number of patents
+            print('\nThe number of patents of ' + name + ' ' + surname + ' is: ' + str(contents['count']));
             return 1;
-        case 'p':                                   # stampa il titolo di ciascun brevetto
+        case 'p':                                   # print the title of each patent
             lista = []
-            for patent in contents['patents']:      # appendi tutti i brevetti ad una lista
+            for patent in contents['patents']:      # append all the patents to a list
                 patent_name = patent['patent_title'];
                 lista.append(patent_name);
-            lista = sorted(list(dict.fromkeys(lista))); # pulisci la lista dai duplicati e mettila in ordine alfabetico
-            print('\nI brevetti di ' + name + ' ' + surname + ' sono:\n');
+            lista = sorted(list(dict.fromkeys(lista))); #clean the list of duplicates and put it in alphabetical order 
+            print('\nI The Patents of ' + name + ' ' + surname + ' are:\n');
             for p in lista:
                 print(p);
             return 1;
 
-def ottieni_brevetti(name, surname):
+def obtain_patents(name, surname):
 
     req = api.Request()
     req.make_request(
-        query=('{"_and":[{"inventor_first_name":["%s"]},'
-               '{"inventor_last_name":["%s"]}]}')
+        query=('{'_and':[{'inventor_first_name':['%s']},'
+               '{'inventor_last_name':['%s']}]}')
                % (name,surname)
     )
     return req.get_data()
-    # try:                                                    # chiamata alle API e traducila in stringa
-    #     contents = urlopen('https://api.patentsview.org/patents/query?q={"_and":[{"inventor_first_name":["' + name + '"]},{"inventor_last_name":["' + surname + '"]}]}').read()
-    #     contents = json.loads(contents)                     # converti in JSON
-    # except HTTPError as e:                                  # catch errori HTTP
+    # try:                                                    # call to the API and translate it to string
+    #     contents = urlopen('https://api.patentsview.org/patents/query?q={'_and':[{'inventor_first_name':['' + name + '']},{'inventor_last_name':['' + surname + '']}]}').read()
+    #     contents = json.loads(contents)                     # convert in JSON
+    # except HTTPError as e:                                  # catch errors HTTP
     #     print('Error code: ', e.code)
-    # except URLError as e:                                   # catch errori URL
+    # except URLError as e:                                   # catch errors URL
     #     print('Reason: ', e.reason)
     # else:
     #     return contents
@@ -45,27 +45,27 @@ if len(sys.argv) == 4:
     name = sys.argv[1]
     surname = sys.argv[2]
     val = sys.argv[3]
-    stampa_brevetti(name, surname, val, ottieni_brevetti(name, surname));
+    print_patents(name, surname, val, obtain_patents(name, surname));
 else:
-    while 1: # ciclo infinito, nel caso sbagliasse a digitare il nome dell'artista
-        name = input("Inserisci il nome dell'artista: ")  # prendo il nome e cognome
-        surname = input("Inserisci il cognome dell'artista: ")  # prendo il nome e cognome
+    while 1: # infinite loop, in case you misspell the artist name
+        name = input('Enter the artist name: ')  # I take the name and surname
+        surname = input('Enter the artist's surname: ')  # I take the name and surname
 
-        if (len(name) == 0) or (len(surname) == 0):                                           # se sbaglia cicla
-            print("\nPer favore, inserisci il Nome e il Cognome dell'artista\n")
+        if (len(name) == 0) or (len(surname) == 0):                                           # if it makes a mistake, it cycles
+            print('\nPlease enter the Name and Surname of the artist\n')
 
         else:
-            contents = ottieni_brevetti(name, surname)
-            exit_code = 0                                       # exit_code è un flag pe ciclare nel caso l'utent e sbagliasse richiesta (p/n)
+            contents = obtain_patents(name, surname)
+            exit_code = 0                                       # exit_code is a flag to cycle in case the user gets the request wrong (p / n)
             while 1:
-                val = input("\nTrovato! Cosa vuoi sapere di questo artista?\n\nI brevetti oppure il numero di brevetti? [p/n]: ")
-                exit_code = stampa_brevetti(name, surname, val, contents);
+                val = input('\nFound it! What do you want to know about this artist?\n\nThe patents or the number of patents?[p/n]: ')
+                exit_code = print_patents(name, surname, val, contents);
                 if exit_code == 1:
                     break
                 else:
-                    print('\nMi dispiace, gli unici valori accettati sono "p" (per i brevetti) ed "n" (per il numero di brevetti)')
+                    print('\n Sorry, the only accepted values ​​are 'p' (for patents) and 'n' (for number of patents)')
             if exit_code == 1:
                 break
 
-input("\nPress Enter to exit...")
+input('\nPress Enter to exit...')
 exit()
