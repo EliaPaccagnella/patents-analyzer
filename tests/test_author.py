97cc9c71211d0d18e22e9a81cd64e8e1422d9a9b
AnnaTonolo <95570933+AnnaTonolo@users.noreply.github.com>
2022-01-23 16:49:51 +0100

    Update test author feature, check PEP8

--- tests/test_author.py
+++ tests/test_author.py
@@ -8,19 +8,19 @@ from analyzer.author_feature import print_patents
 class TestMakeRequestAuthor(unittest.TestCase):
 
     def test_valid_input(self):
-        self.assertEqual(print_patents('Scott','Davidson','n'),1)
-        self.assertEqual(print_patents('Scott','Davidson','p'),1)
+        self.assertEqual(print_patents('Scott', 'Davidson', 'n'), 1)
+        self.assertEqual(print_patents('Scott', 'Davidson', 'p'), 1)
 
     def test_wrong_input(self):
-        self.assertEqual(print_patents('8','Davidson','n'),0)
-        self.assertEqual(print_patents('Scott','7','n'),0)
-        self.assertEqual(print_patents('8','7','n'),0)
-        self.assertEqual(print_patents('Scott','Davidson','a'),0)
+        self.assertEqual(print_patents('8', 'Davidson', 'n'), 0)
+        self.assertEqual(print_patents('Scott', '7', 'n'), 0)
+        self.assertEqual(print_patents('8', '7', 'n'), 0)
+        self.assertEqual(print_patents('Scott', 'Davidson', 'a'), 0)
 
     def test_corner_input(self):
-        self.assertEqual(print_patents('Scott','','n'),0)
-        self.assertEqual(print_patents('Scott','Davidson',''),0)
-        self.assertEqual(print_patents('','Davidson','n'),0)
+        self.assertEqual(print_patents('Scott', '', 'n'), 0)
+        self.assertEqual(print_patents('Scott', 'Davidson', ''), 0)
+        self.assertEqual(print_patents('', 'Davidson', 'n'), 0)
 
 
 if __name__ == '__main__':
