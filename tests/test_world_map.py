d34bbbb46cbd1120a347faae472019e883610996
GiacomoVettore <vett444@gmail.com>
2022-01-23 21:07:13 +0100

    PEP8 update

--- tests/test_world_map.py
+++ tests/test_world_map.py
@@ -1,8 +1,8 @@
-from analyzer.country_feature import world_map
 import unittest
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from analyzer.country_feature import world_map
 
 
 class TestWorldMap(unittest.TestCase):
