b22e00e2baede2d0b649a1fcc8764e01a8f0d6c3
GiacomoVettore <vett444@gmail.com>
2022-01-23 19:17:31 +0100

    PEP8 update

--- tests/test_world_map.py
+++ tests/test_world_map.py
@@ -1,8 +1,8 @@
+from analyzer.country_feature import world_map
 import unittest
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from analyzer.country_feature import world_map
 
 
 class TestWorldMap(unittest.TestCase):
@@ -18,23 +18,23 @@ class TestWorldMap(unittest.TestCase):
         ]
 
         for continent in valid_continents:
-            self.assertEqual(world_map(continent),1)
-        
+            self.assertEqual(world_map(continent), 1)
+
         # input must be valid also if continets are uppercase
         for continent in valid_continents:
-            self.assertEqual(world_map(continent.upper()),1)
-        
+            self.assertEqual(world_map(continent.upper()), 1)
+
         # input must be valid also if continets are capitalized
         for continent in valid_continents:
-            self.assertEqual(world_map(continent.capitalize()),1)
-    
+            self.assertEqual(world_map(continent.capitalize()), 1)
+
     def test_invalid_input(self):
         # continet inputed is not a valid continent
-        self.assertEqual(world_map('Mars',0))
-    
+        self.assertEqual(world_map('Mars', 0))
+
     def test_corner_input(self):
         # oceania is not a valid continent for plotly
-        self.assertEqual(world_map('oceania'),0)
+        self.assertEqual(world_map('oceania'), 0)
 
 
 if __name__ == '__main__':
