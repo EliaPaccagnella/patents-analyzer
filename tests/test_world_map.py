67a04120c5366584a868df22e35e7839ba26bc9f
GiacomoVettore <vett444@gmail.com>
2022-01-23 18:32:25 +0100

    Adding valid inputs tests in test_world_map.py

--- tests/test_world_map.py
+++ tests/test_world_map.py
@@ -5,7 +5,27 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(_file_))))
 from analyzer.country_feature import world_map
 
 class TestWorldMap(unittest.TestCase):
+ def test_valid_input(self):
+        # valid continents
+        valid_continents = [
+            'world',
+            'europe',
+            'asia',
+            'north america',
+            'south america',
+            'africa'
+        ]
 
+        for continent in valid_continents:
+            self.assertEqual(world_map(continent),1)
+        
+        # input must be valid also if continets are uppercase
+        for continent in valid_continents:
+            self.assertEqual(world_map(continent.upper()),1)
+        
+        # input must be valid also if continets are capitalized
+        for continent in valid_continents:
+            self.assertEqual(world_map(continent.capitalize()),1)
 
 
 
