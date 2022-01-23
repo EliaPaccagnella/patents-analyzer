0f18102b0b66bc82e86db86c15e1db7dd6855d64
GiacomoVettore <vett444@gmail.com>
2022-01-23 18:29:46 +0100

    Test set creation for world_map

---
+++ tests/test_world_map.py
@@ -0,0 +1,14 @@
+import unittest
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(_file_))))
+from analyzer.country_feature import world_map
+
+class TestWorldMap(unittest.TestCase):
+
+
+
+
+
+if _name_ == '_main_':
+    unittest.main()
