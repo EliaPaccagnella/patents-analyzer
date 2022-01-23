af379747f2f232712cab8d4b60c345e69f3d17f7
AnnaTonolo <95570933+AnnaTonolo@users.noreply.github.com>
2022-01-23 17:24:31 +0100

    update validating inputs author feature

--- analyzer/author_feature.py
+++ analyzer/author_feature.py
@@ -18,34 +18,39 @@ def print_patents(name, surname, output):
         If "n" will print the count of author's patents, if "p" will print
         the list author's patents' name, else will show an error.
     """
+    # validating inputs
+    if name == '' or surname == '':
+        return 0
+    if name.isalpha() and surname.isalpha() and output.isalpha():
 
-    req = api.Request()
-    req.make_request(
-        query=('{"_and":[{"inventor_first_name":["%s"]},'
-               '{"inventor_last_name":["%s"]}]}') % (name, surname)
-    )
-    contents = req.get_data()
+        req = api.Request()
+        req.make_request(
+            query=('{"_and":[{"inventor_first_name":["%s"]},'
+                   '{"inventor_last_name":["%s"]}]}') % (name, surname)
+        )
+        contents = req.get_data()
 
-    match output:
-        # print number of patents
-        case 'n':
-            print('\nThe number of patents of '
-                  + name
-                  + ' '
-                  + surname
-                  + ' is: ' + str(contents['count'])
-                  )
-            return 1
-        # print the title of each patent
-        case 'p':
-            patents = []
-            # append all the patents to a list
-            for patent in contents['patents']:
-                patent_name = patent['patent_title']
-                patents.append(patent_name)
-            # clean the list of duplicates and put it in alphabetical order
-            patents = sorted(list(dict.fromkeys(patents)))
-            print('\nI The Patents of ' + name + ' ' + surname + ' are:\n')
-            for patent in patents:
-                print(patent)
-            return 1
+        match output:
+            # print number of patents
+            case 'n':
+                print('\nThe number of patents of '
+                      + name
+                      + ' '
+                      + surname
+                      + ' is: ' + str(contents['count'])
+                      )
+                return 1
+            # print the title of each patent
+            case 'p':
+                patents = []
+                # append all the patents to a list
+                for patent in contents['patents']:
+                    patent_name = patent['patent_title']
+                    patents.append(patent_name)
+                # clean the list of duplicates and put it in alphabetical order
+                patents = sorted(list(dict.fromkeys(patents)))
+                print('\nI The Patents of ' + name + ' ' + surname + ' are:\n')
+                for patent in patents:
+                    print(patent)
+                return 1
+    else: return 0
