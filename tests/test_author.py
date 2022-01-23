882123ae3e41a07e437b6bb64c777339f785ede2
AnnaTonolo <95570933+AnnaTonolo@users.noreply.github.com>
2022-01-23 11:44:34 +0100

    Updated test author, fixed PEP8

--- tests/test_author.py
+++ tests/test_author.py
@@ -25,42 +25,42 @@ class TestMakeRequestAuthor(unittest.TestCase):
         self.req.make_request(
             endpoint='patents',
             query='{"_and":['
-                        '{"inventor_first_name":["Scott"]},'
-                        '{"inventor_last_name":["Davidson"]}'
+                  '{"inventor_first_name":["Scott"]},'
+                  '{"inventor_last_name":["Davidson"]}'
                   ']}',
             fields=["patent_id"]
         )
 
         # ---> checking the results
         result = ('{"patents":['
-                        '{"patent_id":"4586708"},'
-                        '{"patent_id":"6628498"},'
-                        '{"patent_id":"6636404"},'
-                        '{"patent_id":"7202770"},'
-                        '{"patent_id":"7367600"},'
-                        '{"patent_id":"7425815"},'
-                        '{"patent_id":"7480718"},'
-                        '{"patent_id":"7550948"},'
-                        '{"patent_id":"7925752"},'
-                        '{"patent_id":"8040345"},'
-                        '{"patent_id":"8291088"},'
-                        '{"patent_id":"8359114"},'
-                        '{"patent_id":"8769006"},'
-                        '{"patent_id":"9032028"},'
-                        '{"patent_id":"9065262"}'
-                '],'
-                '"count":15,'
-                '"total_patent_count":15}')
+                  '{"patent_id":"4586708"},'
+                  '{"patent_id":"6628498"},'
+                  '{"patent_id":"6636404"},'
+                  '{"patent_id":"7202770"},'
+                  '{"patent_id":"7367600"},'
+                  '{"patent_id":"7425815"},'
+                  '{"patent_id":"7480718"},'
+                  '{"patent_id":"7550948"},'
+                  '{"patent_id":"7925752"},'
+                  '{"patent_id":"8040345"},'
+                  '{"patent_id":"8291088"},'
+                  '{"patent_id":"8359114"},'
+                  '{"patent_id":"8769006"},'
+                  '{"patent_id":"9032028"},'
+                  '{"patent_id":"9065262"}'
+                  '],'
+                  '"count":15,'
+                  '"total_patent_count":15}')
         self.assertEqual(self.req._Request__data, json.loads(result))
 
     def test_wrong_input(self):
         with self.assertRaises(errors.SyntaxError):
             self.req.make_request(
                query=('{"_and":['
-                            '{"inventor_first_name":[Scott]},'
-                            '{"inventor_last_name":[Davidson]},'
-                            '{"patent_year":2022}'
-                        ']}'),
+                      '{"inventor_first_name":[Scott]},'
+                      '{"inventor_last_name":[Davidson]},'
+                      '{"patent_year":2022}'
+                      ']}'),
                fields=["patent_id"]
             )
 
@@ -68,10 +68,10 @@ class TestMakeRequestAuthor(unittest.TestCase):
         with patch('sys.stdout', new=io.StringIO()) as fake_stdout:
             self.req.make_request(
                 query=('{"_and":['
-                            '{"inventor_first_name":["Scott"]},'
-                            '{"inventor_last_name":["Davidson"]},'
-                            '{"patent_year":2022}'
-                        ']}'),
+                       '{"inventor_first_name":["Scott"]},'
+                       '{"inventor_last_name":["Davidson"]},'
+                       '{"patent_year":2022}'
+                       ']}'),
                 fields=["patent_id"]
             )
 
