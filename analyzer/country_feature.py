ea6c9057caa97f5f99e1dd79c61964c4e3eea099
luisacomisso <878673@stud.unive.it>
2022-01-23 11:14:18 +0100

    PEP-8 check for country_feature.py

--- analyzer/country_feature.py
+++ analyzer/country_feature.py
@@ -4,14 +4,15 @@ import plotly.graph_objects as go
 import pandas as pd
 from patentsview import api
 
-def print_patents(state, val):
+
+def print_patents(st, val):
 
     """
     Return the list or the count of patents given an assignee's country
     fetched using the API of "patentsview.org".
 
     PARAMETERS:
-    state     : the name of the assignee's country
+    st     : the name of the assignee's country
     val       : the parameter passed as argument, used to print the right
                 result (eg. patents' count or the list of patents' names)
                 If val="n", will print the count of the patents,
@@ -19,35 +20,41 @@ def print_patents(state, val):
                 else will show an error.
     """
     req = api.Request()
-    req.make_request(query='{"assignee_country":["'+ state +'"]}')
+    req.make_request(query='{"assignee_country":["' + st + '"]}')
     contents = req.get_data()
-    # contents = get_patents(state)
-    if type(contents) != NoneType :
+    # contents = get_patents(st)
+    if type(contents) != NoneType:
         match val:
             case 'n':
-                if contents['total_patent_count'] > 0 :
-                    if contents['total_patent_count'] >= 100000 :
-                        print('\nThe amount of patents of ' + pycountry.countries.get(alpha_2=str(state)).name + ' is more than 100.000')
+                if contents['total_patent_count'] > 0:
+                    if contents['total_patent_count'] >= 100000:
+                        print('\nThe amount of patents of ' +
+                              pycountry.countries.get(alpha_2=str(st)).name +
+                              'is more than 100.000')
                         return 100000
                     else:
-                        print('\nThe amount of patents of ' + pycountry.countries.get(alpha_2=str(state)).name + ' is: ' + str(contents['total_patent_count']))
+                        print('\nThe amount of patents of ' +
+                              pycountry.countries.get(alpha_2=str(st)).name +
+                              ' is: ' + str(contents['total_patent_count']))
                         return contents['total_patent_count']
             case 'p':
-                if contents['patents'] != None :
+                if contents['patents'] is not None:
                     lista = []
                     for patent in contents['patents']:
                         patent_name = patent['patent_title']
                         lista.append(patent_name)
                     lista = sorted(list(dict.fromkeys(lista)))
-                    print('\nThe patents of ' + pycountry.countries.get(alpha_2=str(state)).name + ' are:\n')
+                    print('\nThe patents of ' +
+                          pycountry.countries.get(alpha_2=str(st)).name +
+                          ' are:\n')
                     for p in lista:
                         print(p)
                     return 1
             case _:
-                print('The parameter is not correct, please use "n" '+
-                      'to print the amount of patents or "p" for the '+
+                print('The parameter is not correct, please use "n" ' +
+                      'to print the amount of patents or "p" for the ' +
                       'list of patents')
                 return 0
-    print('No patent found, '+
+    print('No patent found, ' +
           'please check the country\'s name or try with another one.')
     return 0
