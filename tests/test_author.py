8b524f94c6279559a84fdf2a07bf247d5b8a85b2
AnnaTonolo <95570933+AnnaTonolo@users.noreply.github.com>
2022-01-23 16:41:54 +0100

    update test author feature

--- tests/test_author.py
+++ tests/test_author.py
@@ -1,93 +1,27 @@
 import unittest
 import os
 import sys
-import json
-from unittest.mock import patch
-import io
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from patent import errors
-from patent import api
+from analyzer.author_feature import print_patents
 
 
 class TestMakeRequestAuthor(unittest.TestCase):
 
-    def setUp(self):
-        '''Before every test this method creates a new Request object'''
-        self.req = api.Request()
-        print("\nInitializing test varibles...")
-        return
-
-    def tearDown(self):
-        '''After every test, this method deletes the Request object'''
-        del self.req
-        print("Tests run, all variables have been deleted.")
-        return
-
     def test_valid_input(self):
-        '''Testing valid inputs for make_request'''
-
-        # testing patents endpoint
-        # ---> making api request
-        self.req.make_request(
-            endpoint='patents',
-            query='{"_and":['
-                  '{"inventor_first_name":["Scott"]},'
-                  '{"inventor_last_name":["Davidson"]}'
-                  ']}',
-            fields=["patent_id"]
-        )
-        # ---> checking the results
-        result = ('{"patents":['
-                  '{"patent_id":"4586708"},'
-                  '{"patent_id":"6628498"},'
-                  '{"patent_id":"6636404"},'
-                  '{"patent_id":"7202770"},'
-                  '{"patent_id":"7367600"},'
-                  '{"patent_id":"7425815"},'
-                  '{"patent_id":"7480718"},'
-                  '{"patent_id":"7550948"},'
-                  '{"patent_id":"7925752"},'
-                  '{"patent_id":"8040345"},'
-                  '{"patent_id":"8291088"},'
-                  '{"patent_id":"8359114"},'
-                  '{"patent_id":"8769006"},'
-                  '{"patent_id":"9032028"},'
-                  '{"patent_id":"9065262"}'
-                  '],'
-                  '"count":15,'
-                  '"total_patent_count":15}')
-        self.assertEqual(self.req._Request__data, json.loads(result))
+        self.assertEqual(print_patents('Scott','Davidson','n'),1)
+        self.assertEqual(print_patents('Scott','Davidson','p'),1)
 
     def test_wrong_input(self):
-        '''Testing wrong-case inputs for make_request'''
-
-        # testing wrong val input
-        with self.assertRaises(errors.SyntaxError):
-            self.req.make_request(
-               query=('{"_and":['
-                      '{"inventor_first_name":[Scott]},'
-                      '{"inventor_last_name":[Davidson]},'
-                      '{"patent_year":2022}'
-                      ']}'),
-               fields=["patent_id"]
-            )
+        self.assertEqual(print_patents('8','Davidson','n'),0)
+        self.assertEqual(print_patents('Scott','7','n'),0)
+        self.assertEqual(print_patents('8','7','n'),0)
+        self.assertEqual(print_patents('Scott','Davidson','a'),0)
 
     def test_corner_input(self):
-        '''Testing corner-case inputs for make_request'''
+        self.assertEqual(print_patents('Scott','','n'),0)
+        self.assertEqual(print_patents('Scott','Davidson',''),0)
+        self.assertEqual(print_patents('','Davidson','n'),0)
 
-        # testing request generating no data
-        with patch('sys.stdout', new=io.StringIO()) as fake_stdout:
-            self.req.make_request(
-                query=('{"_and":['
-                       '{"inventor_first_name":["Scott"]},'
-                       '{"inventor_last_name":["Davidson"]},'
-                       '{"patent_year":2022}'
-                       ']}'),
-                fields=["patent_id"]
-            )
-
-        warning = '\033[93mThere is no data for your query\x1b[37m\n'
-        assert fake_stdout.getvalue() == warning
 
 if __name__ == '__main__':
     unittest.main()
