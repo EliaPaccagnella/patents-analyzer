94c07589cdcc1bfef12c4caa41ff1c7221e5707c
AnnaTonolo <95570933+AnnaTonolo@users.noreply.github.com>
2022-01-24 18:22:52 +0100

    update argparse_patents

--- argparse_patents.py
+++ argparse_patents.py
@@ -4,10 +4,11 @@ from analyzer import author_feature
 from analyzer import country_feature
 import pycountry
 
+
 def make_argparse():
 
     FUNCTION_MAP = {
-        'growth': growth,
+        'graph_growth': growth,
         'patents_from_author': author_feature.print_patents,
         'patents_from_country': country_feature.print_patents,
         'world_map': country_feature.world_map
@@ -25,8 +26,8 @@ def make_argparse():
     for country in list(pycountry.countries):
         country_codes += [country.alpha_2]
     parser.add_argument('-c', '--country', choices=country_codes)
-    parser.add_argument('-s', '--start', type=int, choices=range(1976,2022))
-    parser.add_argument('-e', '--end', type=int, choices=range(1976,2022))
+    parser.add_argument('-s', '--start', type=int, choices=range(1976, 2022))
+    parser.add_argument('-e', '--end', type=int, choices=range(1976, 2022))
 
     # arguments needed for world_map
     parser.add_argument(
@@ -41,12 +42,17 @@ def make_argparse():
             'europe'
             ]
         )
-    
-    # arguments needed for patents_from_country and patents_from_author
+
+    # arguments needed for patents_from_country
     group = parser.add_mutually_exclusive_group()
     group.add_argument('-n', '--number', action='store_true')
     group.add_argument('-p', '--patents', action='store_true')
 
+    # arguments needed for patents_from_author
+    parser.add_argument('-f', '--name')
+    parser.add_argument('-s', '--surname')
+
     args = parser.parse_args()
 
     return args
+    
